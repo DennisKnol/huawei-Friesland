231a0702340471b7b01b47b4480e93b7db8dbd82
Benjamin <B.T.Knol@student.tudelft.nl>
2019-12-30 12:35:10 +0100

    Update Run.py

--- Run.py
+++ Run.py
@@ -3,15 +3,14 @@ from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
 from huawei_lte_api.Connection import Connection
 from huawei_lte_api.api.Sms import Sms
 import sched, time
+
 s = sched.scheduler(time.time, time.sleep)
 
-# connection = Connection('http://192.168.8.1/') For limited access, I have valid credentials no need for limited access
-# connection = AuthorizedConnection('http://admin:MY_SUPER_TRUPER_PASSWORD@192.168.8.1/', login_on_demand=True) # If you wish to login on demand (when call requires authorization), pass login_on_demand=True
+
 def ask_extra_gigs(sc):
-    connection = AuthorizedConnection('http://admin:xxxx@192.168.8.1/')
-    client = Client(connection) # This just simplifies access to separate API groups, you can use device = Device(connection) if you want
-    latest_sms = str(list(list(client.sms.get_sms_list()["Messages"].values())[0][0].values())[3])
-    print(latest_sms)
+    connection = AuthorizedConnection('http://admin:DitIsEenWachtwoord20!8@192.168.8.1/')
+    client = Client(connection)
+    latest_sms = next(iter(next(iter(client.sms.get_sms_list()['Messages'].values()))))['Content']
     if '500MB' in latest_sms:
         print('asking for data')
         for _ in range(0, 5):
@@ -21,7 +20,9 @@ def ask_extra_gigs(sc):
         print('plenty of data left')
         s.enter(600, 1, ask_extra_gigs, (sc,))
     else:
+        client.sms.send_sms([+31611621115], "Onverwacht bericht")
         print('unexpected message')
+
 
 s.enter(1, 1, ask_extra_gigs, (s,))
 s.run()
